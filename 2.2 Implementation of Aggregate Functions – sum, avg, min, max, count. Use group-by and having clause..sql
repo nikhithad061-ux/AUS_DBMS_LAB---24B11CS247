69df85b846b139468d24868269bf4a895b66fd50
Nikhitha Lakkoju <nikhithad061@gmail.com>
2026-06-03 11:12:22 +0530

    Create Implementation of Aggregate Functions and Using group-by and having clause.sql

---
+++ 2.2 Implementation of Aggregate Functions – sum, avg, min, max, count. Use group-by and having clause..sql
@@ -0,0 +1,23 @@
+-- Implementation of Aggregate Functions – sum, avg, min, max, count. Use group-by and having clause.
+select * from company; 
+Select AVG(amount) from company;
+
+Select SUM(amount) from company;
+
+Select Max(amount) from company;
+
+Select Min(amount) from company;
+
+Select Count(*) from company;
+
+select companyn,sum(amount) from company group by companyn;
+
+select companyn,min(amount) from company group by companyn;
+
+select companyn,max(amount) from company group by companyn;
+
+select companyn,count(*) from company group by companyn;
+
+select companyn,count(*) from company group by companyn having count(*)>1;
+
+select companyn,sum(amount) from company group by companyn having sum(amount)>10000;
