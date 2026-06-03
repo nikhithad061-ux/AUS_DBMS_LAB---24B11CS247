2bdbe217189f90a2a271678f4a269fdf5ef74ee3
Nikhitha Lakkoju <nikhithad061@gmail.com>
2026-06-03 11:13:27 +0530

    Create Perform Join Operations.sql

---
+++ 3.1 Perform Join Operations-Natural Join, Equi-Join, Outer Join, Left Outer Join, Right Outer Join, Inner Join and assess the impact of query plans on the performance of join heavy queries.sql
@@ -0,0 +1,24 @@
+--  Perform Join Operations-Natural Join, Equi-Join, Outer Join, Left Outer Join, Right Outer Join, Inner Join and assess the impact of query plans on the performance of join heavy queries.
+-- Inner Join
+select *from tb1 inner join tb2 on tb1.rno=tb2.rno;
+Or use the below query also
+select *from tb1 join tb2 on tb1.rno=tb2.rno;
+
+-- Left Outer Join
+select * from tb1 left outer join tb2 on tb1.rno=tb2.rno;
+
+-- Right outer join
+select * from tb1 right outer join tb2 on tb1.rno=tb2.rno;
+
+-- Natural join
+select *from tb1 natural join tb2;
+
+-- Cross join
+select *from tb1 cross join tb2; or use the below query
+select * from tb1,tb2;
+
+-- Self Join:
+select t1.rno, t2.name from tb1 t1,tb1 t2 where t1.rno=t2.rno;
+
+-- EQUI Join:
+select * from tb1,tb2 where tb1.rno=tb2.rno;
